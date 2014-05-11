7ef24d4ddde33e2599307ac4394d9ba1b9f32a82
Adam Magaluk <AdamMagaluK@gmail.com>
2014-05-11 09:48:17 -0400

    Replaced runtime with config

--- runtime.js
+++ runtime.js
@@ -1,9 +1,2 @@
-var runtime = {};
-runtime.ide = {};
-runtime.ide.path = '/Applications/Arduino.app/Contents/Resources/Java';
-runtime.ide.version = 155;
- 
-runtime.buildExtentions = ['c','cpp','S'];
-runtime.mainExtentions = ['pde','ino'];
-
-module.exports = runtime;
+var config = require('./lib/config')
+module.exports = config;
